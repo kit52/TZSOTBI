9485082083f40c7f313fddf921b382570670237b
Aleksey <lesha.sinitsin@mail.ru>
2021-10-21 13:02:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 10) + 1
+}
 
 function App() {
+  const [value, setValue] = useState("");
+  const [usersFullName, setUsersFullName] = useState([])
+
+  const fetchUsers = async () => {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users")
+    if (response.ok) {
+      let json = await response.json();
+      const num1 = getRandomNumber();
+      let num2 = getRandomNumber();
+      if (num1 == num2) num2 = getRandomNumber()
+      const arr = json.filter(item => item.id != num1 && item.id != num2);
+      const arrayFullName = arr.map(item => {
+        return `${item.name} ${item.username}`
+      })
+      setUsersFullName(arrayFullName)
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    }
+  }
+  console.log(usersFullName);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="wrapper">
+        <h1>1. Лого яндекса</h1>
+        <div className="logo"></div>
+      </div>
+      <div className="wrapper">
+        <h1>2. Анимация подчеркивания текста</h1>
+        <p className="text">
+          <span className="text__anim">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus provident quisquam quae, amet eos at iusto numquam aliquam ratione temporibus reiciendis dolores corrupti ea omnis nihil laudantium pariatur. Doloribus, reprehenderit?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus provident quisquam quae, amet eos at iusto numquam aliquam ratione temporibus reiciendis dolores corrupti ea omnis nihil laudantium pariatur. Doloribus, reprehenderit?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus provident quisquam quae, amet eos at iusto numquam aliquam ratione temporibus reiciendis dolores corrupti ea omnis nihil laudantium pariatur. Doloribus, reprehenderit?</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="wrapper">
+        <h1>3. Get - запрос</h1>
+        <button className="btn" onClick={fetchUsers}>Загрузить пользователей</button>
+        <div className="users">
+          {
+            usersFullName.map((item, i) => {
+              return <p key={i}><span>{i + 1} -</span>{item}</p>
+            })
+          }
+        </div>
+        <div className="wrapper">
+          <h1>4. Контролируемый input</h1>
+          <form className="o">
+            <input className="input"
+              type="text"
+              placeholder="Lorem"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
